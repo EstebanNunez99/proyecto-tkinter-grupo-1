270158a67589d208681efa866e7e0c7c464fde2e
Lorena <Lorena@gmail.com>
2025-11-08 19:03:55 -0300

    agregue cambio reloj

--- RELOJ.py
+++ RELOJ.py
@@ -2,8 +2,8 @@ import tkinter as tk
 import time
 ventana = tk.Tk() 
 ventana.title ('Reloj simple')
-ventana.geometry('400x200')
-reloj = tk.Label( ventana, font= ('Arial', 60), bg = 'blue', fg= 'white')
+ventana.geometry('600x300')
+reloj = tk.Label( ventana, font= ('Broadway', 60), bg = 'pink', fg= 'black')
 
 def hora():
     tiempo_actual = time.strftime ('%H:%M:%S')
